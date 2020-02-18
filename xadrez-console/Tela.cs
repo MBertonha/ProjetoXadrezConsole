6fee7e2ac5b052d963db473e65b8e85e736e3740
Matheus Bertonha <bertonha_2011@hotmail.com>
2020-02-18 15:21:03 -0300

    Criando o tabuleiro e exibindo espaços vazios

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tabu)
+        {
+
+            for(int i = 0; i < tabu.linhas; i++)
+            {
+                for(int j = 0; j < tabu.colunas; j++)
+                {
+                    if(tabu.peca(i,j) == null) //Se não houver peça
+                    {
+                        Console.Write("- ");
+                    }
+                    else                       //Se caso fouver uma peça no tabuleiro
+                    {
+                        Console.Write(tabu.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
