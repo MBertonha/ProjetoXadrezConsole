df04267726b652b3962fa3220fab502d93204ae9
Matheus Bertonha <bertonha_2011@hotmail.com>
2020-02-19 10:10:22 -0300

    Implementação dos movimentos da torre e peão

--- xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez/Peao.cs
@@ -14,5 +14,37 @@ namespace xadrez
         {
             return "P";
         }
+
+        public bool podeMover(Posicao pos)
+        {
+            Peca p = tabu.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matAux = new bool[tabu.linhas, tabu.colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //Acima ++
+            if (qteMovimentos == 0)
+            {
+                pos.definirValores(pos.linha - 2, pos.coluna);
+                if (tabu.posicaoValida(pos) && podeMover(pos))
+                {
+                    matAux[pos.linha, pos.coluna] = true;
+                }
+            }
+            //Acima
+            pos.definirValores(pos.linha - 1, pos.coluna);
+            if (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+            }
+
+            
+            
+            return matAux;
+        }
     }
 }
--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -14,5 +14,68 @@ namespace xadrez
         {
             return "T";
         }
+
+        public bool podeMover(Posicao pos)
+        {
+            Peca p = tabu.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matAux = new bool[tabu.linhas, tabu.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            //Abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            //Direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna= pos.coluna + 1;
+            }
+
+            //Esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            return matAux;
+        }
     }
 }
