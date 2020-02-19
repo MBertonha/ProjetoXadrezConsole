a47a0e92230fa441131097518765fbbca9000b5c
Matheus Bertonha <bertonha_2011@hotmail.com>
2020-02-19 10:19:59 -0300

    Implementado movimento da Dama e Bispo

--- xadrez-console/xadrez/Bispo.cs
+++ xadrez-console/xadrez/Bispo.cs
@@ -14,5 +14,72 @@ namespace xadrez
         {
             return "B";
         }
+
+        public bool podeMover(Posicao pos)
+        {
+            Peca p = tabu.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matAux = new bool[tabu.linhas, tabu.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Nordeste
+            pos.definirValores(pos.linha - 1, pos.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+                pos.linha = pos.linha - 1;
+            }
+
+            //Noroeste
+            pos.definirValores(pos.linha - 1, pos.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+                pos.linha = pos.linha - 1;
+            }
+
+            //Sudoeste
+            pos.definirValores(pos.linha + 1, pos.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+                pos.linha = pos.linha + 1;
+            }
+
+            //Sudeste
+            pos.definirValores(pos.linha + 1, pos.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+                pos.linha = pos.linha + 1;
+            }
+
+            return matAux;
+        }
     }
 }
--- xadrez-console/xadrez/Dama.cs
+++ xadrez-console/xadrez/Dama.cs
@@ -14,5 +14,120 @@ namespace xadrez
         {
             return "D";
         }
-    }
+
+        public bool podeMover(Posicao pos)
+        {
+            Peca p = tabu.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matAux = new bool[tabu.linhas, tabu.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            //Abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            //Direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+
+            //Esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            //Nordeste
+            pos.definirValores(pos.linha - 1, pos.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+                pos.linha = pos.linha - 1;
+            }
+
+            //Noroeste
+            pos.definirValores(pos.linha - 1, pos.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+                pos.linha = pos.linha - 1;
+            }
+
+            //Sudoeste
+            pos.definirValores(pos.linha + 1, pos.coluna - 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+                pos.linha = pos.linha + 1;
+            }
+
+            //Sudeste
+            pos.definirValores(pos.linha + 1, pos.coluna + 1);
+            while (tabu.posicaoValida(pos) && podeMover(pos))
+            {
+                matAux[pos.linha, pos.coluna] = true;
+                if (tabu.peca(pos) != null && tabu.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+                pos.linha = pos.linha + 1;
+            }
+
+            return matAux;
+        }
+     }
 }
